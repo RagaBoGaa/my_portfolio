308083cc6f3d142aca5f6ffc7a695cf7bad9c56d
RagaBoGaa <ragab.magdy59@gmail.com>
2025-08-27 18:53:47 +0300

    Refactor CTAButtons component: update links and standardize quotation marks

--- src/components/Hero/CTAButtons.jsx
+++ src/components/Hero/CTAButtons.jsx
@@ -1,23 +1,23 @@
-import { CTA } from "../../styles/GlobalStyles";
-import { CtaContainer } from "./Hero.style";
+import { CTA } from '../../styles/GlobalStyles';
+import { CtaContainer } from './Hero.style';
 
 const CTAButtons = () => {
   return (
     <CtaContainer>
-      <CTA variation="download">
+      <CTA variation='download'>
         <a
-          href="https://drive.google.com/file/d/1OTkEwShxxO7J9JiLEOCDOo_ekQiciS9Z/view"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://drive.google.com/file/d/10Ow0znr7XrnuTkP8mvw0oqx_zypmBrXS/view'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Download cv
         </a>
       </CTA>
-      <CTA variation="download">
+      <CTA variation='download'>
         <a
-          href="https://www.upwork.com/freelancers/~01bd4db53d971c3393"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://www.upwork.com/freelancers/~01bd4db53d971c3393'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           hire me!
         </a>
